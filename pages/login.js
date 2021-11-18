f784d400d0fc5e0723bfdf1ee2f70b5489133356
Nima Amin <nima.amin.taheri@gmail.com>
2021-11-18 20:59:54 +0100

    auth credential provider added

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Head from 'next/head';
+import { signIn } from 'next-auth/client'
 
 class About extends Component {
     render() {
@@ -12,7 +13,16 @@ class About extends Component {
                 <div className="header">
                     <div className="contact">
                         <h1>Login</h1>
-                        <form action="index.html">
+                            <form action="index.html" method="post" onSubmit={(e) => {
+                                e.preventDefault()
+                                signIn("credentials", {
+                                    redirect: false,
+                                    username: "nima",
+                                    password: "amin",
+                                })
+                                    .then((error) => console.log(error))
+                                    .catch((error) => console.log(error));
+                        }}>
                             <input type="text" placeholder="Enter Your email" name="email" />
                             <input type="password" placeholder="Enter Your password" name="password" />
                             <input type="submit" value="Login" name="submit" id="submit" />
